4444b305eeb5d7f90eec699bfc414971e433331a
JosephHughes14 <josephhughes14@me.com>
2013-12-13 18:01:41 +0000

    Create intro.py

---
+++ intro.py
@@ -0,0 +1,26 @@
+#Sebastian, welcome to python this is a python script/file/code
+#(whatever you want to call it)...
+
+#Using hashes how you make comments, comments are not processed 
+#when the code is run, you can add comments to describe your code 
+#and to make it clear to other people what you are trying to 
+#to do.
+
+'''This is also a type of comment
+   The difference is you can make comments over multiple lines
+   These comments start and end with three quotation marks'''
+   
+#the next line is the first line of code that will be run 
+#when you execute this python file:
+
+x = "Hello World"
+
+#the code above sets the variable x to the string hello world, 
+#we know it is a string because it is in quote marks.
+#printing is a quick way to see what a variable is set too:
+
+print x
+
+#I intend to use commentsin code to build a small project together 
+#that relates to your course (maybe) and we can move this project
+#in any direction!
